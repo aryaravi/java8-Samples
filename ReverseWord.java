03b2ba5146edad757d7efc363d394e75286c8bc5
aryaravi <aryaprasadr@gmail.com>
2017-07-06 11:21:15 -0500

    Add files via upload

---
+++ ReverseWord.java
@@ -0,0 +1,26 @@
+package com.saika;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ReverseWord {
+public static void main(String args[]){
+	
+	String s="i am prasad";
+	System.out.println(s);
+	
+	String s1[] = s.split(" ");
+	
+	for(int i=0; i < s1.length; i++){
+	         //System.out.println(s1[i]);
+	     }
+//ArrayList<String> a=new ArrayList<String>(Arrays.asList(s.split(" ")));
+	
+	
+for(int i=s1.length-1;i>=0;i--){	
+//String  temp=;
+System.out.print(s1[i]);
+System.out.print(" ");
+}
+}
+}
